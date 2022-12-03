5f14c772516d0b2711c025f3806a24a5f607bbd3
Jiang Liu <gerry@linux.alibaba.com>
2022-12-03 22:21:58 +0800

    doc/concepts: add documentation for RAFS filesystem
    Add documentation for RAFS filesystem.
    
    Signed-off-by: Jiang Liu <gerry@linux.alibaba.com>

---
+++ docs/docs/Concepts/chunk-dedup.md
@@ -0,0 +1,4 @@
+---
+sidebar_position: 2
+---
+# Chunk Based Data Deduplication
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,18 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Nydus Documentation',
+  tagline: 'Nydus Image Service Documentation',
+  url: 'https://nydus.dev',
   baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'nydus', // Usually your GitHub org/user name.
+  projectName: 'Nydus Image Service', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -35,10 +35,6 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         // blog: {
         //   showReadingTime: true,
@@ -67,9 +63,9 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Documentation',
+            docId: 'Getting Started/Introduction',
+            label: 'Docs',
+            position: 'right',
           },
           // { to: '/blog', label: 'Blog', position: 'left' },
           {
